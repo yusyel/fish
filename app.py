1fd9e7f194fdf93f046d91e78ef144b14a913fff
yusyel <pirekare@outlook.com>
2023-10-28 01:59:10 +0300

    fix

--- app.py
+++ app.py
@@ -2,58 +2,41 @@ import gradio as gr
 from huggingface_hub import from_pretrained_keras
 from tensorflow.keras.preprocessing.image import load_img
 from tensorflow.keras.preprocessing.image import img_to_array
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.applications.mobilenet_v3 import preprocess_input
+
 import numpy as np
 
 model = from_pretrained_keras("yusyel/fishv2")
 
 
-class_names = [
-    "Black Sea Sprat",
-    "Gilt-Head Bream",
-    "Hourse Mackerel",
-    "Red Mullet",
-    "Red Sea Bream",
-    "Sea Bass",
-    "Shrimp",
-    "Striped Red Mullet",
-    "Trout",
-]
+CLASS=["Black Sea Sprat",
+ "Gilt-Head Bream",
+ "Hourse Mackerel",
+ "Red Mullet",
+ "Red Sea Bream",
+ "Sea Bass",
+ "Shrimp",
+ "Striped Red Mullet",
+ "Trout"]
 
 
 def preprocess_image(img):
-    """
-    Preprocessing Image
-
-    args:
-        img:(object): gradio input image
-
-    returns:(img) np array: numpy array of image
-
-
-    """
-    img = load_img(img, target_size=(249, 249, 3))
-    img = img_to_array(img)
+    img = load_img(img, target_size=(224, 224, 3))
+    img = image.img_to_array(img)
     img = np.expand_dims(img, axis=0)
-    img /= 255.0
+    img = preprocess_input(img)
     print(img.shape)
     return img
 
 
+
 def predict(img):
-    """
-    Predicting Image
-
-    args:
-        img:(np array): numpy array of image
-
-    returns:class_names, and pred (dict): result of model prediction
-
-    """
     img = preprocess_image(img)
     pred = model.predict(img)
     pred = np.squeeze(pred).astype(float)
     print(pred)
-    return dict(zip(class_names, pred))
+    return dict(zip(CLASS, pred))
 
 
 demo = gr.Interface(
